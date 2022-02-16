c7f0b85372f1db1505c49016d06a03ca6e4b3dc0
Ben <benbperry1@gmail.com>
2022-02-15 22:47:58 -0800

    made admin real

--- db/seeds.rb
+++ db/seeds.rb
@@ -90,5 +90,7 @@ Product.create(name: "Smoked Salmon",
 
 Event.create(title: "Bainbridge Farmers Market", time: 'Saturdays 10AM-2PM', months: 'August-December', map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.005508879627!2d-122.52149484876576!3d47.6260241790838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54903eb0b2ad8697%3A0xcfd87a1db8399637!2sBainbridge%20Island%20Farmers%E2%80%99%20Market!5e0!3m2!1sen!2sus!4v1643936244012!5m2!1sen!2sus')
 
+User.create(username: "Ben", password: "1234567", admin: true)
+
 
 puts 'seeds have been planted'
